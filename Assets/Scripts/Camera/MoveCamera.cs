31252d19da8ccf0fda9b521a4be9439269e6d76b
Meithal <hurin8888@gmail.com>
2023-10-10 17:19:17 +0200

    Add keyboard scroll and laptop trackpad scroll, and touchscreen scroll (not tested)

--- Assets/Scripts/Camera/MoveCamera.cs
+++ Assets/Scripts/Camera/MoveCamera.cs
@@ -4,47 +4,99 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    private Camera cam;
+
+    public float ScrollSpeed = 1f;
+    private Camera _cam;
+    private Vector2 _touch;
+
     // Start is called before the first frame update
     void Start()
     {
-        cam = GetComponent<Camera>();
-        Debug.Log(cam.pixelRect);
+        _cam = GetComponent<Camera>();
+        Debug.Log(_cam.pixelRect);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Rect rect = _cam.pixelRect;
+
+        var x = 0f;
+        var y = 0f;
+        Vector3 pos = _cam.transform.position;
+
+        #region mouse
         var mp = Input.mousePosition;
 
-        Rect rect = cam.pixelRect;
-
-        var x = 0;
-        var y = 0;
-        Vector3 pos = cam.transform.position;
-
-        if(mp.x >= 0 && mp.x < 5)
+        if (mp.x >= 0 && mp.x < 5)
         {
-            x = -1;
-        } else if(mp.x <= rect.width && mp.x > rect.width - 5)
+            x = -ScrollSpeed;
+        }
+        else if(mp.x <= rect.width && mp.x > rect.width - 5)
         {
-            x = 1;
+            x = ScrollSpeed;
         }
 
         if (mp.y >= 0 && mp.y < 5)
         {
-            y = -1;
+            y = -ScrollSpeed;
         }
         else if (mp.y <= rect.height && mp.y > rect.height - 15)
         {
-            y = 1;
+            y = ScrollSpeed;
+        }
+        #endregion
+        
+        #region keyboard
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            x = -ScrollSpeed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            x = ScrollSpeed;
         }
 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            y = ScrollSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            y = -ScrollSpeed;
+        }
+        #endregion
+
+        #region TouchScreen
+        if(Input.touchSupported && Input.touchCount == 2)
+        {
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                this._touch = touch.position;
+            }
+            else if(touch.phase == TouchPhase.Moved)
+            {
+                x = touch.position.x - _touch.x;
+                y = touch.position.y - _touch.y;
+            }
+            
+        }
+        #endregion
+
+        #region LaptopTrackPad
+
+        var dtp = Input.mouseScrollDelta;
+        x = -dtp.x;
+        y = dtp.y;
+        #endregion
+        
         if (x != 0 || y != 0)
         {
             pos.x += x;
             pos.z += y;
-            cam.transform.position = pos;
+            _cam.transform.position = pos;
         }
     }
 }
